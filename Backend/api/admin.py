061ebac8de7ea8202c41df937b3f178829bb5ed6
Lorena <210885@utxicotepec.edu.mx>
2024-04-18 13:57:58 -0600

    Modificacion del archivo admin.py

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import tbl_cliente,tbl_rol
 
+admin.site.register(detalles_pedidos)
+admin.site.register(detalles_productos)
+admin.site.register(detalles_promociones)
+admin.site.register(productos)
+admin.site.register(pedidos)
+admin.site.register(promociones)
+
 # Register your models here.
-admin.site.register(tbl_cliente)
-admin.site.register(tbl_rol)
+# admin.site.register(tbl_cliente)
+# admin.site.register(tbl_rol)
 
